b2446c54e02703fae2af1a4ae89507d676d7de4f
Yanchen <fengyc2018@gmail.com>
2018-09-23 14:09:12 -0400

    SPA PART 1: Added Contact page and updated home page

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Card, CardImg, CardTitle, CardSubtitle, CardText, CardBody } from 'reactstrap';
+
+
+function RenderCard({item}) {
+    console.log(item);
+    return (<div>
+            <Card>
+                <CardImg top width='100%' src={item.image} alt={item.name}/>
+                <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    {item.designation? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>);
+}
 
  const Home = (props) => {
+     console.log(props);
     return (
-        <h4>Home</h4>
+        <div className='container'>
+            <div className='row align-items-stretch'>
+                <div className='col-12 col-md m-1'><RenderCard item={props.dish}/></div>
+                <div className='col-12 col-md m-1'><RenderCard item={props.promo}/></div>
+                <div className='col-12 col-md m-1'><RenderCard item={props.leader}/></div>
+            </div>
+        </div>
+        
     );
 }
 
--- src/Components/main.js
+++ src/Components/main.js
@@ -5,14 +5,21 @@ import Header from './header';
 import Footer from './footer';
 import DishDetail from './dishdetail';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 import Home from './home';
+import Contact from './contactus';
 
 export default class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         };
         this.onDishselect = this.onDishselect.bind(this);
     }
@@ -25,15 +32,19 @@ export default class Main extends Component {
     render() {
         const Homepage = (props) => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promo={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
         return (<div>
             <Header />
             <Switch>
-                <Route path="/home" component={() => <Home />}/>
+                <Route path="/home" component={Homepage}/>
                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}
                 onClick={this.onDishselect} />}/>
+                <Route path='/contact' component={Contact} />
                 <Redirect to="/home" />
             </Switch>
             <DishDetail dish={this.state.selectedDish}/>
